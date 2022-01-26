9164e827d8c2e472d7c9df633fb6fe8fe36ecd74
Lesley Lam <lyl13@georgetown.edu>
2022-01-26 18:20:10 -0500

    Asked for user input

--- game.py
+++ game.py
@@ -5,3 +5,32 @@
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+
+
+
+
+# ASK FOR USER INPUT
+user_choice = input("Please select your choice: 'rock', 'paper', or 'scissors': ")
+
+# test
+print("Your choice:", user_choice)
+print(type(user_choice))
+
+# VALIDATIONS
+
+
+
+# COMPUTER CHOICE
+
+
+
+# DETERMINE THE WINNER
+
+
+
+# FINAL RESULTS
+
+
+# BONUS:
+# PLAYER NAME
+# TESTING
