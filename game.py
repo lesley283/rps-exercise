0b393897cf6f7e7b03305d5b1428bf638fe11da1
Lesley Lam <lyl13@georgetown.edu>
2022-01-29 17:31:50 -0500

    Update game.py

--- game.py
+++ game.py
@@ -16,18 +16,30 @@ user_choice = input("Please select your choice: 'rock', 'paper', or 'scissors': 
 # print(type(user_choice))
 
 # VALIDATIONS
-
 # convert user input into all lowercase
 user_choice = user_choice.lower()
 
+# determine if the user entered a valid choice; quit program if NOT
 if user_choice == "rock" or user_choice == "paper" or user_choice == "scissors":
     print("Your choice:", user_choice)
 else:
     print("You entered an invalid choice. The program will now terminate.")
     exit()
 
-
 # COMPUTER CHOICE
+# store possible choices into a list
+possible_choices = ["rock", "paper", "scissors"]
+
+# simulate computer selection using the random module
+import random # loads the module
+computer_choice = random.choice(possible_choices)
+
+#test
+print("Computer choice:", computer_choice)
+print(type(computer_choice))
+
+
+
 
 
 
