d1ac91cc2add1bb8d15cfe427930610f21784a3d
Lesley Lam <lyl13@georgetown.edu>
2022-01-29 17:51:41 -0500

    Update game.py

--- game.py
+++ game.py
@@ -12,19 +12,18 @@
 # ASK FOR USER INPUT
 user_choice = input("Please select your choice: 'rock', 'paper', or 'scissors': ")
 
-# test
-# print(type(user_choice))
 
 # VALIDATIONS
 # convert user input into all lowercase
 user_choice = user_choice.lower()
 
-# determine if the user entered a valid choice; quit program if NOT
+# determine if the user entered a valid choice and print; quit program if NOT
 if user_choice == "rock" or user_choice == "paper" or user_choice == "scissors":
     print("Your choice:", user_choice)
 else:
     print("You entered an invalid choice. The program will now terminate.")
     exit()
+
 
 # COMPUTER CHOICE
 # store possible choices into a list
@@ -34,17 +33,33 @@ possible_choices = ["rock", "paper", "scissors"]
 import random # loads the module
 computer_choice = random.choice(possible_choices)
 
-#test
+# print computer choice
 print("Computer choice:", computer_choice)
-print(type(computer_choice))
-
-
-
-
 
 
 # DETERMINE THE WINNER
+# unless in the case of a tie, for each user choice, determine the possible outcomes based on the computer's choice
+# using nested if / else if statements
+if user_choice == computer_choice:
+    winner = "none" # neither player won
+elif user_choice == "rock":
+    if computer_choice == "paper":
+        winner = "computer"
+    elif computer_choice == "scissors":
+        winner = "user"
+elif user_choice == "paper":
+    if computer_choice == "rock":
+        winner = "user"
+    elif computer_choice == "scissors":
+        winner = "computer"
+elif user_choice == "scissors":
+    if computer_choice == "rock":
+        winner = "computer"
+    elif computer_choice == "paper":
+        winner = "user"
 
+# test
+print(winner)
 
 
 # FINAL RESULTS
